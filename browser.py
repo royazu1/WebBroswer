452cca0ed4d41307abde5c3f2223a967aa4736ca
royazu1 <roeeazu1@gmail.com>
2025-02-10 19:01:27 +0200

    An outline of a basic cmdline script to send an http request from a url and print the HTML text at the server

---
+++ browser.py
@@ -0,0 +1,72 @@
+import string, ssl, socket
+
+class URL: #url example: http://example.org:80/index.html
+    #http://browser.engineering:80/examples/example1-simple.html
+    def __init__(self, url): # fields: protocol, domain, port, path (i.e path to object..)
+        self.protocol , url =  url.split("://",1)
+        assert self.protocol in ["http", "https"]
+        self.domain , url= url.split(":",1)
+        if "/" not in url:
+            url=url + "/"
+        self.port, self.path = url.split("/",1)
+        self.port= int(self.port)
+        self.path= "/" + self.path 
+        
+    def request(self): #use fields genereated in constructor to generate an HTTP request to the server running under the domain..
+        #http request contains the method (POST or GET), the path, the version of http being used, all in the same lines
+        #subsequent lines contain headers with "header name:value" pairs, each pair in a line.
+        request= "GET "+ self.path + " HTTP/1.0" + "\r\n"
+        request+= "Host: " + self.domain +"\r\n"
+        request+="\r\n" #empty line to signify the end of the headers section
+        
+        #create a socket
+        sock= socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=socket.IPPROTO_TCP) #af_inet stands for address family internet
+        sock.connect((self.domain, self.port))
+        if self.protocol == "https":
+            ssl_context = ssl.create_default_context()
+            sock= ssl_context.wrap_socket(sock) #wrap in the ssl cntx to provide a secure HTTP connection
+        sock.send(request.encode("utf8"))
+        
+        #The response first line holds the protocol, reponse code, response message. Example: "HTTP/1.0 200 OK"
+        #Subsequent lines are response headers. After these lines there is the body that contains the HTML code.
+        response= sock.makefile(mode="r", encoding="utf8", newline="\r\n") #convert the response to a string
+        line= response.readline()
+        resp_protocol, resp_code, resp_msg= line.split(" ", 2)
+        resp_msg= resp_msg.strip()
+        print(resp_msg)
+        assert resp_msg == "OK"
+        
+        resp_headers= {} #dic
+        while True: 
+            line= response.readline()
+            if line == "\r\n":
+                break
+            header, val = line.split(":",1)
+            resp_headers[header.casefold()]= val.strip()
+        
+        assert "transfer-encoding" not in resp_headers
+        assert "content-encoding" not in resp_headers #we assert here that there is no compression of data from or to us
+        #no what's left to read from response is the response's body
+        resp_body= response.read() #read whatever data are left..
+        return resp_body
+    
+def show(resp_body): #display only the text within the HTML body returned by the request() method
+        in_tags= False
+        output=""            
+        for c in resp_body:
+            if c == '<' and not in_tags:
+                in_tags=True    
+            elif c == '>' and in_tags:
+                in_tags=False
+            elif not in_tags:
+                output+=c     
+        print(output)
+        
+def load_web_page(url : URL):
+    body=url.request()
+    show(body)    
+
+
+if __name__ == "__main__":
+    import sys
+    show(URL(sys.argv[1]).request())
